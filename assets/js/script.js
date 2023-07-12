40e1ccb19fad8cdb1a4509aeb7c02549513565db
jsck45 <132333536+jsck45@users.noreply.github.com>
2023-07-12 21:01:29 +1000

    5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,7 @@ function handleSubmit(event) {
       if (latitude && longitude) {
         
         getWeather(city, latitude, longitude); 
+        getForecast(city,latitude, longitude);
       } else {
         console.log('Latitude or longitude not found in the API response.');
       }
@@ -42,8 +43,8 @@ function getWeather(city, latitude, longitude) {
     .then(data => {
       var weatherDiv = document.getElementById('weather');
       weatherDiv.innerHTML = `
-      <h2>The current weather in ${city}:</h2>
-      <p>Temperature: ${data.main.temp}°C</p>
+      <h3>The current weather in ${city}:</h3>
+      <p>Temperature: ${data.main.temp}&deg;C</p>
       <p>Humidity: ${data.main.humidity}%</p>
       <p>Wind Speed: ${(data.wind.speed * 3.6).toFixed(0)}km/h</p>
       `;
@@ -52,5 +53,56 @@ function getWeather(city, latitude, longitude) {
       console.log('Error:', error);
     });
 }
+
+function getForecast(city, latitude, longitude) {
+  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=d460e8dccd9357124bd36c9cb150895c&units=metric`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      var forecastDaysDiv = document.getElementById('forecastDays');
+      forecastDaysDiv.innerHTML = "";
+      var forecastByDay = {};
+
+      for (let index = 0; index < data.list.length; index++) {
+        var forecast = data.list[index];
+        var forecastTime = new Date(forecast.dt * 1000);
+        var forecastDate = forecastTime.toLocaleDateString('en-GB').split('/').reverse().join('');
+
+        if (forecastByDay[forecastDate]) {
+          continue;
+        }
+
+        forecastByDay[forecastDate] = forecast;
+
+        // var icon = forecast.weather[0].icon;
+        var humidity = forecast.main.humidity;
+        var windSpeed = forecast.wind.speed * 3.6;
+        var temperature = forecast.main.temp;
+        var description = forecast.weather[0].description;
+
+        var forecastElement = document.createElement("div"); // Create a div container for each day's forecast
+        forecastElement.classList.add("forecast-box"); // Add a CSS class for styling
+
+        var iconCode = forecast.weather[0].icon; // Get the icon code
+        var iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`; // Construct the icon URL
+
+        forecastElement.innerHTML = `
+          <p><strong>${forecastTime.toLocaleDateString()}</p>
+          <img src="${iconUrl}" alt="Weather Icon"> 
+          <p>${description}</p>
+          <p>Temp: ${temperature.toFixed(0)}&deg;C</p>
+          <p>Humidity: ${humidity}%</p>
+          <p>Wind Speed: ${windSpeed.toFixed(0)} km/h</p>
+        `;
+
+        forecastDaysDiv.appendChild(forecastElement);
+      }
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
+
 
 document.getElementById('geocodingForm').addEventListener('submit', handleSubmit);
